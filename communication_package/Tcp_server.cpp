a81b42eb8c88831b46e5310b0ffa9f9ac28385b7
houssainy <mohamed.houssainy@hotmail.com>
2014-11-08 16:53:21 +0200

    First commit

---
+++ communication_package/Tcp_server.cpp
@@ -0,0 +1,11 @@
+#include "Tcp_server.h"
+
+Tcp_server::Tcp_server()
+{
+    //ctor
+}
+
+Tcp_server::~Tcp_server()
+{
+    //dtor
+}
---
+++ communication_package/Tcp_server.h
@@ -0,0 +1,14 @@
+#ifndef TCP_SERVER_H
+#define TCP_SERVER_H
+
+
+class Tcp_server
+{
+    public:
+        Tcp_server();
+        virtual ~Tcp_server();
+    protected:
+    private:
+};
+
+#endif // TCP_SERVER_H
